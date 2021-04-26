6687de4ddee3f0e8adbd699400ec8dc03a5f6ccc
matdan <matdangariepy@hotmail.com>
2021-04-26 10:59:21 -0400

    finished install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-for entry in "."/*
+for entry in *
 do
-	if [ $entry = "./install.sh" ]; then
+	if [ $entry = "install.sh" ]; then
 		continue
 	fi
-	ln -sf $(pwd)/$entry $HOME
+	ln -sf $(pwd)/.$entry $HOME/.$entry
 	echo $(pwd)/$entry
 done
 
